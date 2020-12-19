473d9a9faf3846f0868acc8fff6a483eb625dc53
shasank112001 <shasanksekharpandey@gmail.com>
2020-12-19 14:47:50 +0100

    combined checkRow and checkColumn into one function. Added the check for the right diagonals.

---
+++ src/GUI/GameSetUpPanel.java
@@ -0,0 +1,15 @@
+package GUI;
+
+import javax.swing.*;
+
+public class GameSetUpPanel extends JPanel {
+    private JTextField gridSize;
+    private JTextField name1;
+    private JTextField name2;
+    private JLabel NameTextField;
+    private JLabel gridSizeTextField;
+
+    public GameSetUpPanel(){
+
+    }
+}
--- src/TicTac/Board.java
+++ src/TicTac/Board.java
@@ -81,50 +81,8 @@ public class Board {
         return true;
     }
 
-//    public boolean checkDiagonal(Mark m){
-//        boolean check=false;
-//        int i,j;
-//        for(i=0;i<size;i++){
-//            if(!fields[i][i].equals(m)){
-//                break;
-//            }
-//        }
-//        if(i==size){ return true;}
-//        for(i=0,j=size-1;i<size;i++,j--){
-//            if(!fields[i][j].equals(m)){
-//                break;
-//            }
-//        }
-//        if(i==size){ return true;}
-//        return false;
-//    }
-
-//    public boolean checkDiagonal(Mark m){
-//        int i, j, counter = 0;
-//        for(i = 0 ; i < size; i++){
-//            if(fields[i][i].equals(m)){
-//                counter++;
-//                if( counter == this.winningNum){ return true;}
-//
-//            } else counter = 0;
-//        }
-//
-//        counter=0;
-//
-//        for(i = 0, j= this.size-1; i<size ; i++, j--){
-//            if(fields[i][j].equals(m)){
-//                counter++;
-//                if( counter == this.winningNum){ return true;}
-//            } else counter = 0;
-//        }
-//
-//        return false;
-//
-//
-//    }
-
     public boolean checkDiagonal(Mark m){
-        int i, j, counter = 0, counter1 = 0;
+        int i, j,k, counter = 0, counter1 = 0;
         for(i = 0; i<= size-winningNum ; i++){
             for( j = 0; j< size - i ; j++){
                 if(fields[j][j+i].equals(m)){
@@ -139,66 +97,45 @@ public class Board {
                 } else counter1 = 0;
             }
         }
-        return false;
-
-    }
-//    public boolean checkRow(Mark m){
-//        int i,j;
-//        for(i=0;i<size;i++){
-//            for(j=0; j<size;j++) {
-//                if (!fields[i][j].equals(m)) {
-//                    break;
-//                }
-//            }
-//            if(j==size){ return true;}
-//        }
-//        return false;
-//    }
-
-    public boolean checkRow(Mark m){
-        int i, j, counter;
-        for(i=0;i<size;i++){
-            counter = 0;
-            for(j=0; j<size;j++) {
-                if (fields[i][j].equals(m)) {
+        counter = counter1 = 0;
+        for(i = 0; i <= size - winningNum ; i++){
+            for( j = size-1,k=0; j >= i; j--,k++){
+                if(fields[i+k][j].equals(m)){
                     counter++;
                     if(counter == this.winningNum){ return true;}
-                } else counter=0;
+                } else counter = 0;
+
+                if(fields[k][j-i].equals(m)){
+                    counter1++;
+                    if(counter1 == this.winningNum){ return true;}
+                } else counter1 = 0;
             }
         }
         return false;
+
     }
 
-//    public boolean checkColumn(Mark m){
-//        int i,j;
-//        for(i=0;i<size;i++){
-//            for(j=0;j<size;j++){
-//                if (!fields[j][i].equals(m)) {
-//                    break;
-//                }
-//            }
-//            if(j==size){ return true;}
-//        }
-//        return false;
-//    }
-
-    public boolean checkColumn(Mark m){
-        int i, j, counter;
+    public boolean checkColumnAndRow(Mark m){
+        int i, j, counter,counter1;
         System.out.println(this.toString());
         for(i=0;i<size;i++){
             counter = 0;
+            counter1 = 0;
             for(j=0;j<size;j++){
                 if (fields[j][i].equals(m)) {
                     counter ++;
                     if(counter == this.winningNum){ return true;}
                 } else counter = 0;
+                if (fields[i][j].equals(m)) {
+                    counter1++;
+                    if(counter1 == this.winningNum){ return true;}
+                } else counter1=0;
             }
         }
         return false;
     }
-
     public boolean checkMarkForWin(Mark m){
-        return checkColumn(m) || checkDiagonal(m) || checkRow(m);
+        return checkColumnAndRow(m) || checkDiagonal(m);
     }
 
     public boolean hasWinner(){
